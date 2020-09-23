ee31f30fd8a7e68b377a61a3a9ca0eb03f3bd4cf
crystalfn <969274162@qq.com>
2020-09-23 23:15:44 +0800

    [fangni] feat: exercise-2 - implement product page

--- src/exercise-1/components/Products/Product/product.js
+++ src/exercise-1/components/Products/Product/product.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
-import '../../../../exercise-2/mockups/data.json';
+import React, { Component } from "react";
+import data from "../../../../exercise-2/mockups/data.json";
+import './product.css';
 
 class Prosuct extends Component {
+  state = {
+    details: {},
+  };
+
+  componentDidMount() {
+    const index = parseInt(this.props.match.url.split("/")[2]);
+    for (const key in data) {
+      if (data[key].id === index) {
+        this.setState({
+          details: data[key],
+        });
+      }
+    }
+  }
+
   render() {
     return (
-      <section>
-        product
+      <section className="product">
+        <h3>Product Details:</h3>
+        <ul>
+          <li>Name: {this.state.details.name}</li>
+          <li>Id: {this.state.details.id}</li>
+          <li>Price: {this.state.details.price}</li>
+          <li>Quantity: {this.state.details.quantity}</li>
+          <li>Desc: {this.state.details.desc}</li>
+          <li>URL: {this.props.match.url}</li>
+        </ul>
       </section>
     );
   }
